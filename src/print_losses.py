f0f92f87f1f4e6833757db8e155d3707307f4d6f
Bruno Brant <1890900+bruno-brant@users.noreply.github.com>
2022-10-11 11:13:01 -0300

    feat: create a helper to check loss for models

---
+++ src/print_losses.py
@@ -0,0 +1,31 @@
+import os
+
+import torch
+
+from money_counter import constants
+
+print(os.getcwd())
+
+
+def print_losses(folder: str):
+    best = (float('inf'), None)
+
+    for file in os.listdir(folder):
+        path = os.path.join(folder, file)
+        if os.path.isfile(path):
+            if file.endswith('.pth'):
+                with open(path, 'rb') as f:
+                    model = torch.load(f)
+                    print(f'Loss for {path}:', model['loss'])
+
+                    if best is None or best[0] > model['loss']:
+                        best = (model['loss'], path)
+
+        elif os.path.isdir(path):
+            print_losses(path)
+
+    if best[1] is not None:
+        print(f'Best model for {folder}:', best[1])
+
+
+print_losses(constants.MODEL_STATE_DIR)
